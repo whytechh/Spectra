8213992bcac0f25fcaad6d25086adc729c493310
chusov.e <chusoveg17@gmail.com>
2025-04-20 21:07:12 +0500

    add validation dataset creation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,7 +2,8 @@ import os.path
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from common_functions import parse_all_files_info, save_as_train_dataset, save_as_test_dataset, load_train_dataset
+from common_functions import parse_all_files_info, save_as_train_dataset, save_as_test_dataset, load_train_dataset, \
+    save_as_validation_dataset
 from paths import result_dataset_directory, preprocessed_data_directory
 
 file_infos = parse_all_files_info(result_dataset_directory)
@@ -12,15 +13,20 @@ os.makedirs(preprocessed_data_directory, exist_ok=True)
 df.to_csv(os.path.join(preprocessed_data_directory, 'all_dataset.csv'), index=False)
 train_dfs = []
 test_dfs = []
+validation_dfs = []
 grouped = df.groupby('name')
 for x in grouped.groups:
     group = grouped.get_group(x)
-    train_df, test_df = train_test_split(group, test_size=0.3)
+    train_df, test_df = train_test_split(group, test_size=0.2)
+    train_df, validation_df = train_test_split(train_df, test_size=0.25)
     train_dfs.append(train_df)
     test_dfs.append(test_df)
+    validation_dfs.append(validation_df)
 
 train_df = pd.concat(train_dfs)
 test_df = pd.concat(test_dfs)
+validation_df = pd.concat(validation_dfs)
 
 save_as_train_dataset(train_df)
 save_as_test_dataset(test_df)
+save_as_validation_dataset(validation_df)
--- paths.py
+++ paths.py
@@ -1,6 +1,7 @@
 preprocessed_data_directory = 'preprocessed_data'
 train_dataset_name = 'train_dataset.csv'
 test_dataset_name = 'test_dataset.csv'
+validation_dataset_name = 'validation_dataset.csv'
 
 spx_directory = r'C:\Users\Egor\Desktop\study\project\CalibrationFiles_25470_25480'
 result_dataset_directory = r'C:\Users\Egor\Desktop\study\project\result_dataset'
