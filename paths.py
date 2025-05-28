27b5609494b84d84e3f95633005033395368712e
chusov.e <chusoveg17@gmail.com>
2025-05-28 22:15:57 +0500

    make dataset for spx files

--- paths.py
+++ paths.py
@@ -5,3 +5,4 @@ validation_dataset_name = 'validation_dataset.csv'
 
 spx_directory = r'C:\Users\Egor\Desktop\study\project\CalibrationFiles_25470_25480'
 result_dataset_directory = r'C:\Users\Egor\Desktop\study\project\result_dataset'
+result_spx_dataset_directory = r'C:\Users\Egor\Desktop\study\project\CalibrationFiles_25470_25480'
